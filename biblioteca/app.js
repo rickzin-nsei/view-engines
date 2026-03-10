0684d31416b681774a803eb862d4289cee66d43a
Henrique P <hpereirasergio@gmail.com>
2026-03-10 15:04:54 -0300

    2º

--- biblioteca/app.js
+++ biblioteca/app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var novaRouter = require('./routes/nova');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/nova', novaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
